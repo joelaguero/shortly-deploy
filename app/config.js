1c1314d88b1b3fd045948d2b5d9222360fbc7414
Hack Reactor Students <accounts@hackreactor.com>
2016-03-31 12:10:15 -0700

    Define urls and users schemas, require autoIncrement

--- app/config.js
+++ app/config.js
@@ -1,17 +1,35 @@
 var path = require('path');
 var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var autoIncrement = require('mongoose-auto-increment');
 
 mongoose.connect('mongodb://localhost:27017', function(err) {
   if (err) { console.log(err); }
   console.log('Connected to MongoDB database');
 });
 
-var Schema = mongoose.Schema;
+autoIncrement.initialize(connection);
+
 
 // Create urls schema
-
+var urlsSchema = new Schema({
+  id: Number,
+  url: String,
+  baseUrl: String,
+  code: String,
+  title: String,
+  visits: Number,
+  timestamps: { type: Date, default: Date.now }
+}); 
 
 // Create user schema
+var usersSchema = new Schema({
+  id: Number,
+  userName: String,
+  password: String,
+  timestamps: { type: Date, default: Date.now }
+});
+
 
 
 // Knex and db can be removed eventually.
