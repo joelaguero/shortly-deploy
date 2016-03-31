c91429e06772c790b7a3d69674738111de9f8f3e
Hack Reactor Students <accounts@hackreactor.com>
2016-03-30 17:08:09 -0700

    Add templates for gitadd, gitpush, gitcommit

--- Gruntfile.js
+++ Gruntfile.js
@@ -53,6 +53,23 @@ module.exports = function(grunt) {
       prodServer: {
       }
     },
+
+    gitadd: {
+      task: {
+        options: {
+          force: true
+        },
+        files: {
+          src: ['test.txt']
+        }
+      }
+    },
+
+    gitcommit: {
+    },
+
+    gitpush: {
+    }
   });
 
   grunt.loadNpmTasks('grunt-contrib-uglify');
@@ -63,6 +80,7 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-mocha-test');
   grunt.loadNpmTasks('grunt-shell');
   grunt.loadNpmTasks('grunt-nodemon');
+  grunt.loadNpmTasks('grunt-git');
 
   grunt.registerTask('server-dev', function (target) {
     // Running nodejs in a different process and displaying output on the main console
